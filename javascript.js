9a9588e2275f37a3fe390b04f4a7feb27f2749a8
FriFeb <51440204+FriFeb@users.noreply.github.com>
2023-09-27 20:04:36 +0300

    feat: create 16x16 grid

---
+++ javascript.js
@@ -0,0 +1,29 @@
+/* 
+create container with 960x960 px
+set gridSize to 16 (it means 16x16 grid within container)
+go into a loop:
+
+    go into a loop:
+
+        create div with 960 / 16 px size
+        append to the grid
+
+		continue until iterations are over (columnSize < 16)
+
+continue until iterations are over (rowSize < 16)
+*/
+
+const gridSize = 16;
+const containerSize = 960;
+
+const container = document.querySelector(".container");
+container.style.width = container.style.height = containerSize + "px";
+
+for (let i = 0; i < gridSize; i++) {
+  for (let j = 0; j < gridSize; j++) {
+    const div = document.createElement("div");
+    div.className = "grid-element";
+    div.style.width = div.style.height = containerSize / gridSize + "px";
+    container.appendChild(div);
+  }
+}
